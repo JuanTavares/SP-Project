7fbef6e286b1e5ad9893b50730affcb5efb3d8fa
Juan Tavares <juantavares2006@hotmail.com>
2018-10-25 17:51:11 -0300

    Creating user-tab component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UserTabComponent } from './home/user-tab/user-tab.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UserTabComponent
   ],
   imports: [
     BrowserModule,
